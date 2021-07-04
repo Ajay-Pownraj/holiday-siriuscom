3d2395db46e55ae1f0757f23511297d1ef1c9137
Ajay Pownraj <ajaypownraj@gmail.com>
2021-07-04 17:07:13 +0530

    Adding Loader and  planner gif to README.md

--- src/containers/destination/Destination.jsx
+++ src/containers/destination/Destination.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import './Destination.css';
 import axios from 'axios';
+import { BeatLoader } from 'react-spinners';
 const Destination = () => {
     const [dest, getDest] = useState([])
+    const [isloading, setloading] = useState(false)
     const url = "https://run.mocky.io/v3/3e6901dd-9a60-4771-a8cb-9c62177a654c"
     useEffect(() => {
+        setloading(true)
         axios.get(url)
             .then(res => {
                 getDest(res.data.result)
@@ -12,6 +15,7 @@ const Destination = () => {
             .catch(e => {
                 console.log(e)
             })
+        setloading(false)
     }, [])
     console.log("Des-->", dest)
     return (
@@ -23,11 +27,16 @@ const Destination = () => {
                 {dest.map((cardDatas, i) => (
                     <div key={i}>
                         <div className="dest-img-content" >
-                            <img
-                                className="city-img"
-                                src={cardDatas.imageUrl}
-                                alt={cardDatas.city}
-                            />
+                            {isloading
+                                ?
+                                <BeatLoader color='#7554a0' loading={isloading} />
+                                :
+                                <img
+                                    className="city-img"
+                                    src={cardDatas.imageUrl}
+                                    alt={cardDatas.city}
+                                />
+                            }
                         </div>
                         <div className="city-name" key={cardDatas.city}>{cardDatas.city}</div>
                     </div>
